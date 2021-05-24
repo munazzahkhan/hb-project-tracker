ed2bbfe2d3aa4da21142e32c4e6a9e2dc301958b
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-05-24 14:02:56 -0700

    Finished Lab Excercise

---
+++ hackbright.sql
@@ -0,0 +1,240 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.2
+-- Dumped by pg_dump version 13.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: grades; Type: TABLE; Schema: public; Owner: munazzahhassankhan
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO munazzahhassankhan;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: munazzahhassankhan
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO munazzahhassankhan;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: munazzahhassankhan
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
+-- Name: projects; Type: TABLE; Schema: public; Owner: munazzahhassankhan
+--
+
+CREATE TABLE public.projects (
+    id integer NOT NULL,
+    title character varying(30),
+    description text,
+    max_grade integer
+);
+
+
+ALTER TABLE public.projects OWNER TO munazzahhassankhan;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE; Schema: public; Owner: munazzahhassankhan
+--
+
+CREATE SEQUENCE public.projects_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.projects_id_seq OWNER TO munazzahhassankhan;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: munazzahhassankhan
+--
+
+ALTER SEQUENCE public.projects_id_seq OWNED BY public.projects.id;
+
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: munazzahhassankhan
+--
+
+CREATE TABLE public.students (
+    id integer NOT NULL,
+    first_name character varying(30),
+    last_name character varying(30),
+    github character varying(30)
+);
+
+
+ALTER TABLE public.students OWNER TO munazzahhassankhan;
+
+--
+-- Name: students_id_seq; Type: SEQUENCE; Schema: public; Owner: munazzahhassankhan
+--
+
+CREATE SEQUENCE public.students_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.students_id_seq OWNER TO munazzahhassankhan;
+
+--
+-- Name: students_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: munazzahhassankhan
+--
+
+ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
+
+
+--
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: munazzahhassankhan
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
+-- Name: projects id; Type: DEFAULT; Schema: public; Owner: munazzahhassankhan
+--
+
+ALTER TABLE ONLY public.projects ALTER COLUMN id SET DEFAULT nextval('public.projects_id_seq'::regclass);
+
+
+--
+-- Name: students id; Type: DEFAULT; Schema: public; Owner: munazzahhassankhan
+--
+
+ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.students_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: munazzahhassankhan
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhack	Markov	10
+2	jhack	Blockly	2
+3	sdevelops	Markov	50
+4	sdevelops	Blockly	100
+5	sdevelops	Dictionaries	100
+6	jhack	Dictionaries	20
+7	jhacks	Dictionaries	20
+8	jhacks	Blockly	2
+9	jhacks	Markov	10
+\.
+
+
+--
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: munazzahhassankhan
+--
+
+COPY public.projects (id, title, description, max_grade) FROM stdin;
+1	Markov	Tweets generated from Markov chains	50
+2	Blockly	Programmatic Logic Puzzle Game	100
+3	Guessing Game	To play the Number Guessing Game	50
+4	Shopping Site	Trading Cards Site	150
+5	Dctionaries	For Restaurant Rating	100
+\.
+
+
+--
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: munazzahhassankhan
+--
+
+COPY public.students (id, first_name, last_name, github) FROM stdin;
+1	Jane	Hacker	jhacks
+2	Sarah	Developer	sdevelops
+\.
+
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: munazzahhassankhan
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 9, true);
+
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: munazzahhassankhan
+--
+
+SELECT pg_catalog.setval('public.projects_id_seq', 5, true);
+
+
+--
+-- Name: students_id_seq; Type: SEQUENCE SET; Schema: public; Owner: munazzahhassankhan
+--
+
+SELECT pg_catalog.setval('public.students_id_seq', 2, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: munazzahhassankhan
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: munazzahhassankhan
+--
+
+ALTER TABLE ONLY public.projects
+    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: munazzahhassankhan
+--
+
+ALTER TABLE ONLY public.students
+    ADD CONSTRAINT students_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
